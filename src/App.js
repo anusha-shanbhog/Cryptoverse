331e0d0fb2fb87e15aa1cc337ec967340eb6c132
anusha-shanbhog <anushapshanbhog@gmail.com>
2022-07-08 15:48:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Space,Typography,Layout } from 'antd';
+import React from 'react';
+import {Routes,Route, Link} from 'react-router-dom';
+
+
+import {Navbar,Homepage, News, Cryptocurrencies, Exchanges, CryptoDetails} from './components';
 import './App.css';
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+            <div className="navbar">
+<Navbar/>
+
+</div>
+<div className="main">
+<Layout>
+    
+    <div className="routes">
+    <Routes>
+        <br/>
+        <br/><br/>
+        <Route path="/" exact={true} element={<Homepage />} />
+        <Route path="/exchanges" exact={true} element={<Exchanges />} />
+        <Route path="/cryptocurrencies" exact={true} element={<Cryptocurrencies />} />
+        <Route path="/crypto/:coinId" exact={true} element={<CryptoDetails />} />
+        <Route path="/news" exact={true} element={<News />} />
+      </Routes>
+                    
+    </div>
+</Layout>
+
+
+<div className="footer" >
+    
+<Typography.Title level={5} style={{color:'white',textAlign:'center'}}>
+    Cryptoverse<br/>
+All rights reserved
+</Typography.Title>
+<Space>
+    <Link to="/">Home</Link>
+    <Link to="/exchanges">Exchanges</Link>
+    <Link to="/news">News</Link>
+</Space>
+</div>
+</div>
     </div>
   );
 }
 
-export default App;
+export default app
